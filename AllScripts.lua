9386b23a06bc79903d0c89fff4e37f88f80ba6f1
CasperFlyModz <62319070+CasperFlyModz@users.noreply.github.com>
2022-04-02 07:37:52 -0400

    Small Updates

--- AllScripts.lua
+++ AllScripts.lua
@@ -1,1 +1,25 @@
--- this is nothing currently
+if game.PlaceId == "8967359816" then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/24kGoldnConcertExperience.lua"))()
+elseif game.PlaceId == "537413528" then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/BuildABoatForTreasure.lua"))()
+elseif game.PlaceId == "8479386309" then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/EggHunt2022LostinTime.lua"))()
+elseif game.PlaceId == "340809482" then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/LimitedSimulator.lua"))()
+elseif game.PlaceId == "5736409216" then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/MallTycoon.lua"))()
+elseif game.PlaceId == "6884319169" then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/MicUp.lua"))()
+elseif game.PlaceId == "4902872857" then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/PetZooTycoon.lua"))()
+elseif game.PlaceId == "294790062" then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/Titanic.lua"))()
+elseif game.PlaceId == "6766156863" then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/StrongmanSimulator.lua"))()
+elseif game.PlaceId == "8232438881" then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/ThiefSimulator.lua"))()
+elseif game.PlaceId == "70501379" then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/TradeHangout.lua"))()
+elseif game.PlaceId == "54865335" then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/UltimateDriving.lua"))()
+end
