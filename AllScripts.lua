66dc80985a0871e57a1c68d2d156b504ac944e71
CasperFlyModz <62319070+CasperFlyModz@users.noreply.github.com>
2022-04-05 22:11:16 -0400

    Update AllScripts.lua

--- AllScripts.lua
+++ AllScripts.lua
@@ -13,7 +13,7 @@ elseif game.PlaceId == "6884319169" then
 elseif game.PlaceId == "4902872857" then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/PetZooTycoon.lua"))()
 elseif game.PlaceId == "294790062" then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/Titanic.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/RobloxTitanic.lua"))()
 elseif game.PlaceId == "6766156863" then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/StrongmanSimulator.lua"))()
 elseif game.PlaceId == "8232438881" then
@@ -22,4 +22,20 @@ elseif game.PlaceId == "70501379" then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/TradeHangout.lua"))()
 elseif game.PlaceId == "54865335" then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/CasperFlyModz/discord.gg-rips/main/UltimateDriving.lua"))()
+elseif game.PlaceId == "7860844204" then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/vfvo/life-sentence/main/lifesentence.lua"))()
+elseif game.PlaceId == "8328351891" then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/vfvo/megamansions-tycoon/main/megamansiontycoon.lua"))()
+elseif game.PlaceId == "6490016198" then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/vfvo/slayer-tycoon/main/sds.lua"))()
+elseif game.PlaceId == "5023820864" then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/vfvo/trade-tower/main/tradesim.lua"))()
+elseif game.PlaceId == "7009799230" then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/vfvo/scripts/main/pressurewashingsim.lua"))()
+elseif game.PlaceId == "6170705260" then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/vfvo/scripts/main/blendingsim2.lua"))()
+elseif game.PlaceId == "4872321990" then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Mxxss/RBLX-Scripts/main/Islands.lua"))()
+elseif game.PlaceId == "6783611814" then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Mxxss/RBLX-Scripts/main/Build%20a%20Boat%20to%20Survive.lua"))()
 end
