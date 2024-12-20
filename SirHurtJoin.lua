94b477f8f61385e5c4d9923b9c4f680ab18ccd97
CasperFlyModz <62319070+CasperFlyModz@users.noreply.github.com>
2024-12-20 18:53:38 -0500

    didnt need the function anymore

--- SirHurtJoin.lua
+++ SirHurtJoin.lua
@@ -1,24 +1,4 @@
-local Invite = {}
-Invite["Vanity"] = "sirhurtv5"
-local HTTPService = game:GetService("HttpService")
-Invite["Join"] = function()
-	coroutine.wrap(pcall)(function()
-		setclipboard("https://discord.gg/" .. Invite)
-		coroutine.wrap((http and http.request) or http_request)({
-			Url = "http://127.0.0.1:6463/rpc?v=1",
-			Method = "POST",
-			Headers = {
-				["Content-Type"] = "application/json",
-				Origin = "https://discord.com"
-			},
-			Body = HTTPService:JSONEncode({
-				cmd = "INVITE_BROWSER",
-				nonce = HTTPService:GenerateGUID(false),
-				args = {
-					code = Invite
-				}
-			})
-		})
-	end)
-end
+local Invite = {
+	Vanity = "sirhurtv5"
+}
 return Invite
